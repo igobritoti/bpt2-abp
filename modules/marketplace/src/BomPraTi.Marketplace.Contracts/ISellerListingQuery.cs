c7ef9259b7ca72cf9a421b062a4a8b4c4a8e8a7c
tihotm <igorbrito.n1@hotmail.com>
2026-08-23 14:53:49 -0300

    Expose owned Listing edit read

--- modules/marketplace/src/BomPraTi.Marketplace.Contracts/ISellerListingQuery.cs
+++ modules/marketplace/src/BomPraTi.Marketplace.Contracts/ISellerListingQuery.cs
@@ -5,4 +5,5 @@ namespace BomPraTi.Marketplace.Contracts;
 public interface ISellerListingQuery : IApplicationService
 {
     Task<IReadOnlyList<ListingDto>> GetMineAsync(CancellationToken cancellationToken = default);
+    Task<SellerListingDetailDto?> GetMineByIdAsync(Guid listingId, CancellationToken cancellationToken = default);
 }
