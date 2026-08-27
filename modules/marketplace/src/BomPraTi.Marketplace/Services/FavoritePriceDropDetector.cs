01f367e6ad0d932cf9d1acba1fc3a54387a62e81
igobritoti <igorbrito.ti@gmail.com>
2026-08-27 11:26:48 -0300

    Filter price-drop replay by Favorite creation time

--- modules/marketplace/src/BomPraTi.Marketplace/Services/FavoritePriceDropDetector.cs
+++ modules/marketplace/src/BomPraTi.Marketplace/Services/FavoritePriceDropDetector.cs
@@ -35,7 +35,10 @@ public sealed class FavoritePriceDropDetector : ITransientDependency
         var favoritesQuery = await _favorites.GetQueryableAsync();
         var favoriteUserIds = await favoritesQuery
             .AsNoTracking()
-            .Where(x => x.ListingId == priceChange.ListingId)
+            .Where(x =>
+                x.ListingId == priceChange.ListingId
+                && x.CreatedAtUtc.HasValue
+                && x.CreatedAtUtc.Value <= priceChange.ChangedAtUtc)
             .Select(x => x.UserId)
             .Distinct()
             .ToListAsync(cancellationToken);
